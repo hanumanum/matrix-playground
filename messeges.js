7b05315553e0f82ce04bbdfeaf736e20610ee727
artak kolyan (aka hanuman) <a.kolyan@gratun.am>
2018-12-28 09:20:07 +0000

    fr translation

--- messeges.js
+++ messeges.js
@@ -25,29 +25,29 @@ let messagesHY ={
     booleans:"Booleans",
     numbers:"Numbers",
     responses:{
-      correct:"Correct: You found that !!!",
+      correct:"Correct!",
       incorrectOutofrange:"Incorrect: Your index is out of the range. Index must be less than ",
-      incorrectEmpty:"Incorrect: Empty, write indexes",
-      incorrect:"Incorrect: Try again ",
+      incorrectEmpty:"Incorrect: Empty, remember to write the indexes",
+      incorrect:"Incorrect: Try again",
     }
   }
   
   let messagesFR ={
-    text1:"We have this matrix",
-    text2:"Find indexes of highlighted value",
-    text3:"You also can try another matrixes",
-    check:"Check",
-    color:"Color Strings",
-    strings:"Strings",
-    booleans:"Booleans",
-    numbers:"Numbers",
-    responses:{
-      correct:"Correct: You found that !!!",
-      incorrectOutofrange:"Incorrect: Your index is out of the range. Index must be less than ",
-      incorrectEmpty:"Incorrect: Empty, write indexes",
-      incorrect:"Incorrect: Try again ",
+    texte1:"Ceci est notre matrix",
+    texte2:"Trouves les indexes des valeurs sur-lignées ",
+    texte3:"Tu peux aussi essayer d’autres matrixes",
+    check:"Vérifie",
+    couleur:"series de couleurs ",
+    séries:"Séries ",
+    booleans:"Booléennes ",
+    numbers:"Nombres",
+    responses:{  
+      correct:"Correcte!",
+      incorrectOutofrange :"Incorrecte: Ton index est hors de l’évantail. L'index doit être inférieur à cela ",
+      incorrectEmpty:"Incorrecte: Vide, souviens-toi d’écrire les indexes",
+      incorrect:"Incorrecte: essaie encore  ",
     }
-  }
+  }    
   
   let messagesRU ={
     text1:"We have this matrix",
